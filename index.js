d1281c77d38214d295f0565c92536879966850fe
= <kevinmassongo196@gmail.com>
2023-09-07 22:58:28 +0200

    finished

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+// const img = document.getElementById('img')
+// fetch('https://api.thecatapi.com/v1/images/search')
+//   .then(response => response.json())
+//   .then(data => img.src = data[0].url)
 
+const img = document.getElementById('img')
+
+fetch('https://api.thecatapi.com/v1/images/search')
+ .then(response => {
+  if(response.ok){
+    response.json().then(data=>img.src = data[0].url)
+  }else{
+    console.log('oups');
+  }
+ })
